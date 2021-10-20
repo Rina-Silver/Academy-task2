05721ce9673cbb62faaa16f766af4a8bad1797b3
Унжакова Екатерина <katerina.unz@gmail.com>
2021-10-20 18:27:59 +0700

    Update markupForm.js

--- js/markupForm.js
+++ js/markupForm.js
@@ -1,5 +1,5 @@
 import refs from './refs.js';
-const { fileNameRef } = refs;
+const { fileNameRef, formContainerRef } = refs;
 function markupForm(data) {
   if (data.name) {
     const firstLetter = data.name.charAt(0).toUpperCase();
@@ -7,8 +7,27 @@ function markupForm(data) {
     fileNameRef.textContent = formName.replace(formName.charAt(0), firstLetter);
   }
   if (data.fields) {
-    console.log(data.fields);
+    markupFields(data.fields);
   }
+}
+function markupFields(data) {
+  data.map(el =>
+    Object.entries(el).forEach(([key, value]) => {
+      let markup = '';
+      if (value.isArray()) {
+        console.log(value);
+        Object.entries(value).forEach(([key, value]) =>
+          console.log(key, value),
+        );
+      } else {
+        markup = `<form class="form-floating">
+        <label for="floatingInputValue">${value}</label>
+        <input type="" class="form-control" id="floatingInputValue" placeholder="" value="">
+</form>`;
+      }
+      formContainerRef.insertAdjacentHTML('beforeend', markup);
+    }),
+  );
 }
 
 export default markupForm;
